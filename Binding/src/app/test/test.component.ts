a274219772b84e196650ae6b5748e211d55496cf
AKHRIFJaafar <akhrif.jaafar.dev@gmail.com>
2022-08-10 02:50:08 +0100

    property_binding

--- Binding/src/app/test/test.component.ts
+++ Binding/src/app/test/test.component.ts
@@ -9,6 +9,8 @@ export class TestComponent implements OnInit {
   
   public name = "Jaafar";
   public siteUrl = window.location.href;
+  public myId = "testId";  
+  public isDisabled = true;
   constructor() { }
 
   ngOnInit(): void {
